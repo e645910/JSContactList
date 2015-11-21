22ed6280154d41bafb495536c5e38bb32dca636b
rwightman <rick.wightman@q.com>
2015-11-21 16:29:10 -0700

    save and remove table working

--- js/contact.js
+++ js/contact.js
@@ -68,24 +68,40 @@ var Contacts = {
 				phone: this.phone.value,
 				email: this.email.value
 			};
-		if (Contacts.$form.setFocus === null) {
-			Contacts.storeAdd(entry);
-			Contacts.tableAdd(info);
-		}
-		else { 
-			Contacts.storeAdd(entry)
-			// ==========write code to handle table view here
-			Contacts.tableAdd(info);
+			function getTableEntry(newEntry) {
+				var newTable = [];
+				newEntry.forEach(function(query) {
+					if (companyName === query.company) {
+					 	newTable.push({
+							"id"		: query.id,
+							"fullname"	: query.fullname,
+							"dept"		: query.dept,
+							"phone"		: query.phone,
+							"email"		: query.email,
+							"notes"		: query.notes
+						})
+					}
+				})
+				removeTableRows();
+				return newTable;
+			};
+			var companyName = this.company.value;
+			var newTable = getTableEntry(contacts_list);
 
-			// Contacts.storeEdit(entry);
-			// Contacts.tableEdit(entry);
-		}
-		this.reset();
-		Contacts.$select.value = '';
-		this.idEntry.value = 0;
-		setFocus();
-		event.preventDefault();
-	}, true);
+			if (entry.id == 0) {
+					Contacts.storeAdd(entry);
+					newTable.forEach(Contacts.tableAdd)
+					}
+			else { 
+				Contacts.storeEdit(entry);
+				newTable.forEach(Contacts.tableEdit)
+			}
+			this.reset();
+			Contacts.$select.value = '';
+			this.idEntry.value = 0;
+			setFocus();
+			event.preventDefault();
+		}, true);
 
 // ==================== initialize the DOM table =====================================
 
@@ -97,35 +113,16 @@ var Contacts = {
 					contacts_list.push(JSON.parse(window.localStorage.getItem(key)));
 				}
 			}
-
-			// function getTableView(view){
-			// 	var tableView = [];
-			// 	view.forEach(function(query){
-			// 		if (query.company === selectCompany) {
-			// 			tableView.push({
-			// 			"id" 		: query.id,
-			// 			"fullname"	: query.fullname,
-			// 			"dept"		: query.dept,
-			// 			"phone"		: query.phone,
-			// 			"email"		: query.email,
-			// 			"notes"		: query.notes
-			// 			})
-			// 		}
-			// 	})
-			// 	return tableView;
-			// };
-			// var tableView = getTableView(contacts_list)
-			// tableView.forEach(Contacts.tableAdd)
 		};
 
 // ==================== initialize the dropdown list ================================= 
-		function getCompanyName(names){
+		function getCompanyName(names) {
 			var companyName = [];
-			names.forEach(function(query){
+			names.forEach(function(query) {
 				companyName.push(query.company);
 			})
 			Contacts.$select.add( new Option(''));
-			return companyName.sort().filter(function(item, position, array){
+			return companyName.sort().filter(function(item, position, array) {
 				return !position || item != array[position - 1];
 			});
 		};
@@ -135,12 +132,12 @@ var Contacts = {
 			Contacts.$select.add( new Option(companyName[name]));
 			}
 
-		Contacts.$select.onchange = function(){
+		Contacts.$select.onchange = function() {
     		var selectCompany = Contacts.$select.options[Contacts.$select.selectedIndex].value;
     		removeTableRows();
 
 //========================== create filtered array linked to dropdown ===   
-		    function getSelectedCompany(info){
+		    function getSelectedCompany(info) {
 				var selectedInfo = [];
 				info.forEach(function(query) {
 					if (query.company === selectCompany) {
@@ -167,59 +164,59 @@ var Contacts = {
 	    	selectedInfo.forEach(Contacts.tableAdd)
 
 // ==================== sort contacts ========================================
-			// var sortOrderAscending = true;
-			// function scopePreserver() {
-			// 	return function() {
-			// 	var string = this.innerText;
-			// 	var tableHeader = string.toLowerCase();
+			var sortOrderAscending = true;
+			function scopePreserver() {
+				return function() {
+				var string = this.innerText;
+				var tableHeader = string.toLowerCase();
 
-			// 		function sortOn(arr, prop, reverse) {
-			// 			if (!prop || !arr) {
-			// 				return arr;
-			// 			}
+					function sortOn(arr, prop, reverse) {
+						if (!prop || !arr) {
+							return arr;
+						}
 
-			// 			function sort_by(field, rev, primer) {
-			// 				return function(a, b) {
-			// 					a = primer(a[field]),
-			// 					b = primer(b[field]);
+						function sort_by(field, rev, primer) {
+							return function(a, b) {
+								a = primer(a[field]),
+								b = primer(b[field]);
 
-			// 					if (sortOrderAscending === false){
-			// 						return ((a < b) ? 1 : ((a > b) ? -1 : 0)) * (rev ? 1 : 1);
-			// 					}
-			// 					return ((a < b) ? -1 : ((a > b) ? 1 : 0)) * (rev ? -1 : 1);
-			// 				}
-			// 			}
-			// 			if (tableHeader === 'id') {
-			// 				arr.sort(sort_by(prop, reverse, function(a) {
-			// 				return parseFloat(String(a).replace(/[^0-9.-]+/g, ''));
-			// 				}));
-			// 			}else {	
-			// 				arr.sort(sort_by(prop, reverse, function(a){
-			// 				return String(a).toUpperCase();
-			// 				}));
-			// 			}
-			// 		};
+								if (sortOrderAscending === false) {
+									return ((a < b) ? 1 : ((a > b) ? -1 : 0)) * (rev ? 1 : 1);
+								}
+								return ((a < b) ? -1 : ((a > b) ? 1 : 0)) * (rev ? -1 : 1);
+							}
+						}
+						if (tableHeader === 'id') {
+							arr.sort(sort_by(prop, reverse, function(a) {
+							return parseFloat(String(a).replace(/[^0-9.-]+/g, ''));
+							}));
+						}else {	
+							arr.sort(sort_by(prop, reverse, function(a) {
+							return String(a).toUpperCase();
+							}));
+						}
+					};
 
-			// 		if (tableHeader !== 'actions') {
-			// 			removeTableRows();
+					if (tableHeader !== 'actions') {
+						removeTableRows();
 						
-			// 		    sortOrderAscending === true ? sortOrderAscending = false: sortOrderAscending = true;
+					    sortOrderAscending === true ? sortOrderAscending = false: sortOrderAscending = true;
 
-			// 		    function sortTable(){
-			// 		    	sortOn(selectedInfo, tableHeader, false, false);
-			// 				selectedInfo.forEach(Contacts.tableAdd)
-			// 		    }sortTable();
-			// 		}
-			// 	};
-			// };
+					    function sortTable() {
+					    	sortOn(selectedInfo, tableHeader, false, false);
+							selectedInfo.forEach(Contacts.tableAdd)
+					    }sortTable();
+					}
+				};
+			};
 
-			// function tableContainScope() {
-			// var titles = document.getElementsByTagName('th');
-			// var rows = document.getElementsByTagName('tr');
-			// 	for( var i = 0; i < titles.length; i++) {
-			// 		titles[i].onclick = scopePreserver( i, rows[i]);
-			// 	}
-			// }tableContainScope();
+			function tableContainScope() {
+			var titles = document.getElementsByTagName('th');
+			var rows = document.getElementsByTagName('tr');
+				for( var i = 0; i < titles.length; i++) {
+					titles[i].onclick = scopePreserver( i, rows[i]);
+				}
+			}tableContainScope();
 		};
 
 // == add event listener then determine which callback function was triggered ======
@@ -227,26 +224,25 @@ var Contacts = {
 		Contacts.$table.addEventListener("click", function(event) {
 			var op = event.target.getAttribute("data-op");
 			if (/edit|remove/.test(op)) {
-				var entry = JSON.parse(window.localStorage.getItem("Contacts:"+ event.target.getAttribute("data-id")));
+				var record = JSON.parse(window.localStorage.getItem("Contacts:"+ event.target.getAttribute("data-id")));
 				if (op === "edit") {
-					Contacts.$form.company.value = entry.company;
-					Contacts.$form.address1.value = entry.address1;
-					Contacts.$form.address2.value = entry.address2;
-					Contacts.$form.city.value = entry.city;
-					Contacts.$form.state.value = entry.state;
-					Contacts.$form.zip.value = entry.zip;
-					Contacts.$form.notes.value = entry.notes;
-					Contacts.$form.fullname.value = entry.fullname;
-					Contacts.$form.dept.value = entry.dept;
-					Contacts.$form.phone.value = entry.phone;
-					Contacts.$form.email.value = entry.email;
-					Contacts.$form.idEntry.value = entry.id;
+					Contacts.$form.company.value = record.company;
+					Contacts.$form.address1.value = record.address1;
+					Contacts.$form.address2.value = record.address2;
+					Contacts.$form.city.value = record.city;
+					Contacts.$form.state.value = record.state;
+					Contacts.$form.zip.value = record.zip;
+					Contacts.$form.notes.value = record.notes;
+					Contacts.$form.fullname.value = record.fullname;
+					Contacts.$form.dept.value = record.dept;
+					Contacts.$form.phone.value = record.phone;
+					Contacts.$form.email.value = record.email;
+					Contacts.$form.idEntry.value = record.id;
 				}
 				else if (op === "remove") {
-					if (confirm('Are you sure you want to remove "'+ entry.fullname + ' with '+ entry.company + '" from your contacts?')) {
-						Contacts.storeRemove(entry);
-						Contacts.tableRemove(entry);
-						setFocus();
+					if (confirm('Are you sure you want to remove "'+ record.fullname + ' with '+ record.company + '" from your contacts?')) {
+						Contacts.storeRemove(record);
+						Contacts.tableRemove(record);
 					}
 				}
 				event.preventDefault();
@@ -263,45 +259,43 @@ var Contacts = {
 		},
 		storeEdit: function(entry) {
 			window.localStorage.setItem("Contacts:"+ entry.id, JSON.stringify(entry));
-			setFocus();
 		},
 		storeRemove: function(entry) {
 			window.localStorage.removeItem("Contacts:"+ entry.id);
 		},
 
 // =============================== table build =====================================
-		tableAdd: function(info) {
+		tableAdd: function(newTable) {
 			var $tr = document.createElement("tr"), $td, key;
-			for (key in info) {
-				if (info.hasOwnProperty(key)) {
+			for (key in newTable) {
+				if (newTable.hasOwnProperty(key)) {
 					$td = document.createElement("td");
-					$td.appendChild(document.createTextNode(info[key]));
+					$td.appendChild(document.createTextNode(newTable[key]));
 					$tr.appendChild($td);
 				}
 			}
 			$td = document.createElement("td");
-			$td.innerHTML = '<a data-op="edit" data-id="'+ info.id +'">Edit</a> | <a data-op="remove" data-id="'+ info.id +'">Remove</a>';
+			$td.innerHTML = '<a data-op="edit" data-id="'+ newTable.id +'">Edit</a> | <a data-op="remove" data-id="'+ newTable.id +'">Remove</a>';
 			$tr.appendChild($td);
-			$tr.setAttribute("id", "info-"+ info.id);
+			$tr.setAttribute("id", "newTable-"+ newTable.id);
 			Contacts.$table.appendChild($tr);
 		},
-		tableEdit: function(entry) {
-			var $tr = document.getElementById("entry-"+ entry.id), $td, key;
+		tableEdit: function(newTable) {
+			var $tr = document.getElementById("newTable-"+ newTable.id), $td, key;
 			$tr.innerHTML = "";
-			for (key in entry) {
-				if (entry.hasOwnProperty(key)) {
+			for (key in newTable) {
+				if (newTable.hasOwnProperty(key)) {
 					$td = document.createElement("td");
-					$td.appendChild(document.createTextNode(entry[key]));
+					$td.appendChild(document.createTextNode(newTable[key]));
 					$tr.appendChild($td);
 				}
 			}
 			$td = document.createElement("td");
-			$td.innerHTML = '<a data-op="edit" data-id="'+ entry.id +'">Edit</a> | <a data-op="remove" data-id="'+ entry.id +'">Remove</a>';
+			$td.innerHTML = '<a data-op="edit" data-id="'+ newTable.id +'">Edit</a> | <a data-op="remove" data-id="'+ newTable.id +'">Remove</a>';
 			$tr.appendChild($td);
 		},
-		tableRemove: function(info) {
-			// console.log(entry, entry.id)
-			Contacts.$table.removeChild(document.getElementById("info-"+ info.id));
+		tableRemove: function(newTable) {
+			Contacts.$table.removeChild(document.getElementById("newTable-"+ newTable.id));
 		}
 };
 Contacts.init();
